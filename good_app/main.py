8ab8937c55926ef77eb2bb27bb1395cf513b38fe
Stef <s.kraaijeveld3@student.avans.nl>
2025-09-16 17:08:25 +0200

    Basic functionality and README change

--- good_app/main.py
+++ good_app/main.py
@@ -4,16 +4,34 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("/html/home.html")
+    return render_template("html/home.html")
 
-@app.route("/about")
-def about():
-    return render_template("/html/about.html")
+@app.route("/bias")
+def bias():
+    return render_template("html/bias.html")
 
-@app.route("/submit", methods=["POST"])
-def submit():
-    name = request.form.get("username")
-    return render_template("/html/greeting.html", name=name)
+@app.route("/explainability")
+def explainability():
+    return render_template("html/explainability.html")
+
+@app.route("/privacy")
+def privacy():
+    return render_template("html/privacy.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    option1 = request.form.get("dropdown1")
+    option2 = request.form.get("dropdown2")
+    option3 = request.form.get("dropdown3")
+    user_input = request.form.get("inputField")
+
+    return render_template(
+        "html/result.html",
+        option1=option1,
+        option2=option2,
+        option3=option3,
+        user_input=user_input
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
